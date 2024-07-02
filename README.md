2211c60e6a0280e779f4d8ca843219bda59a95ef
Mickey Perlstein <corp.mp@gmail.com>
2024-07-02 23:33:42 +0300

    added more tests and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# working_catch_testing
+# A working example of Catch framework working correctly with Visual studio Catch2 Test adapter
+
+As an experienced systems architect I am used to just starting at a running pace, with presetup systems working as planned and examples and demos that just work.
+I have had the need to go back to Cpp, and my memories are not fond from my CS degree.
+
+I am working on a windows system.
+I tried installing many types and versions of cpp on this computer and they all had an issue and didnt want to work.
+I decided to default back to visual studio, and installed visual studio 2022.
+
+I then looked for a testing framework that did not require an expternal package manager of submodaling a git (such as google tests).
+I found catch2.hpp as a single dependency and was very happy.
+
+Here is how to run this simple testing:
+
+1. clone the repo to your directory of choice
+1. install visual studio 2022
+1. open the extensions and install [**Catch2 Test Adapter** extension](https://marketplace.visualstudio.com/items?itemName=JohnnyHendriks.ext01)
+1. Go to [Test]->[Configure run Settings]->[Select Solutions Wide runsettings File]
+     select test.runsettings located in test_project
+
+1. Clean and Build the solution
+1. Open test explorer and (hopefully) all the tests (3 tests) will appear there._
--- test_project/TestCases.cpp
+++ test_project/TestCases.cpp
@@ -3,19 +3,55 @@
 #include "catch.hpp"  
 #include "../system_under_test/BankAccount.hpp"
 
+TEST_CASE("Deposit money into an account", "[deposit]") {
+	GIVEN("an empty bank account") {
+		Bank_Account s;
+
+		WHEN("I deposit 10.000000") {
+			std::string expected_message = "10.000000 will be deposited.";
+			std::string actual_message = s.deposit(10.000000);
+
+			THEN("the system should display \"10.000000 will be deposited.\"") {
+				REQUIRE(actual_message == expected_message);
+			}
+		}
+	}
+}
+
+
+TEST_CASE("Deposit money into an account _sections", "[deposit]") {
+	SECTION("Given an empty bank account");
+	Bank_Account s;
+
+	SECTION("When I deposit 10.000000");
+	std::string expected_message = "10.000000 will be deposited.";
+	std::string actual_message = s.deposit(10.000000);
+
+	SECTION("Then the system should display \"10.000000 will be deposited.\"");
+	REQUIRE(actual_message == expected_message);
+
+}
+
 TEST_CASE("Testing Deposit") {//Testing the deposit
-    Bank_Account s = Bank_Account();
-    REQUIRE(s.deposit(10.000000) == "10.000000 will be deposited.");
+
+	Bank_Account s;
+	REQUIRE(s.deposit(10.000000) == "10.000000 will be deposited.");
 }
 
 TEST_CASE("Testing Withdrawal") {//Testing the withdrawal
-    Bank_Account s = Bank_Account();
-    REQUIRE(s.withdraw(50.000000) == "50.000000 will be withdrawn.");
+	Bank_Account s = Bank_Account();
+	REQUIRE(s.withdraw(50.000000) == "50.000000 will be withdrawn.");
 }
 
+TEST_CASE("Testing Withdrawal2") {//Testing the withdrawal
+	Bank_Account s = Bank_Account();
+	REQUIRE(s.withdraw(50.000000) == "50.000000 will be withdrawn.");
+}
+
+
 TEST_CASE("Testing Display") {//Testing the display
-    Bank_Account s = Bank_Account();
-    REQUIRE(s.deposit(50.000000) == "50.000000 will be deposited.");
-    REQUIRE(s.withdraw(10.000000) == "10.000000 will be withdrawn.");
-    REQUIRE(s.display() == "Your Available Balance = 40.000000");
+	Bank_Account s = Bank_Account();
+	REQUIRE(s.deposit(50.000000) == "50.000000 will be deposited.");
+	REQUIRE(s.withdraw(10.000000) == "10.000000 will be withdrawn.");
+	REQUIRE(s.display() == "Your Available Balance = 40.000000");
 }
